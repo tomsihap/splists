13cbd810ea3c46e50d89c26f24aa647b3b254ae0
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-01-11 11:23:36 +0100

    Creating files

---
+++ index.php
@@ -0,0 +1,23 @@
+- index.php
+
+- views/
+        login.php
+        signup.php
+        board.php
+
+- controllers/
+        loginController.php
+        signupController.php
+        boardController.php
+        helper.php
+
+- partials/
+        _header.php
+        _footer.php
+        _form_ticket.php
+        _view_ticket.php
+
+- assets/
+        js/
+        css/
+        img/
